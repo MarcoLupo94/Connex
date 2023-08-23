f02feb45700e22d27b6e3111df7e8aeba6b2703f
marco <marco.lupo@missionlabs.co.uk>
2023-08-23 13:51:09 +0100

    cleanup

--- server/router.ts
+++ server/router.ts
@@ -1,10 +1,8 @@
 import express, { Router } from 'express';
 import { getTime } from './controller';
-import { Validator, ValidationError } from 'express-json-validator-middleware';
 
 const router: Router = express.Router();
-const { validate } = new Validator();
 
-router.get('/time', validate({ body: './schemas/Time.json' }), getTime);
+router.get('/time', getTime);
 
 export default router;
